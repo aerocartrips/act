e06ba0bf040604e877237ed750cd2226a2a5f70f
--g <aerocartripsservices@gmail.com>
2025-11-05 08:10:40 +0530

    about page mobile fixed

--- app/about/page.js
+++ app/about/page.js
@@ -7,7 +7,7 @@ export default function Page() {
 
       <div className="min-h-[95vh] flex justify-center items-center">
         <div>
-          <h1 className="text-6xl text-center">Aerocartrips Private Limited</h1>
+          <div className="text-4xl md:text-6xl text-center">Aerocartrips Private Limited</div>
           <p className="text-center py-8">
             We are enthusiastic to provide help, support, and guidance to
             everyone, ensuring satisfaction outcomes always.
